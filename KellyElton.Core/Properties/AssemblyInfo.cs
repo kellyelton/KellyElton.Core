5429ea90fe5040753623486fe9c8aca3957e6545
Kelly Elton <its.the.doc@gmail.com>
2016-10-22 19:35:12 -0400

    Updated some info

--- KellyElton.Core/Properties/AssemblyInfo.cs
+++ KellyElton.Core/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("KellyElton.Core")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription( "KellyElton.Core" )]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany( "Kelly Elton" )]
 [assembly: AssemblyProduct("KellyElton.Core")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright( "Copyright © Kelly Elton 2016" )]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
