f545c8b5ddcbe4dc09c98a1ff64c454d223168fa
Kelly Elton <its.the.doc@gmail.com>
2016-10-22 17:33:27 -0400

    Start

---
+++ KellyElton.Core/IModule.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KellyElton
+{
+    public interface IModule : IComponent
+    {
+        IEnumerable<IComponent> Components { get; }
+    }
+}
